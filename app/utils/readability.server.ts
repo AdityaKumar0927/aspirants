2ae30627d81cb0c0144db95d430860e730ee3ac1
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2025-04-06 22:15:49 -0500

    add parseContentWithReadability export to fix missing export error in readbility.server.ts

--- app/utils/readability.server.ts
+++ app/utils/readability.server.ts
@@ -1,12 +1,11 @@
 // app/utils/readability.server.ts
-import { JSDOM } from "jsdom";
-import { Readability } from "mozilla-readability";
+import { JSDOM } from "jsdom"
+import { Readability } from "mozilla-readability"
 
-export function parseContentWithMozillaReadability(url: string, html: string): string {
-  const dom = new JSDOM(html);
-  const doc = dom.window.document;
-
-  const reader = new Readability(url, doc);
-  const parsed = reader.parse();
-  return parsed?.textContent || "No content extracted.";
+export function parseContentWithReadability(url: string, html: string): string {
+  const dom = new JSDOM(html)
+  const doc = dom.window.document
+  const reader = new Readability(url, doc)
+  const parsed = reader.parse()
+  return parsed?.textContent || "No content extracted."
 }
