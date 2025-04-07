3e2173fa2d0b94d99d46b7d739f8ef99a94b7100
Aditya Kumar <107320645+AdityaKumar0927@users.noreply.github.com>
2025-04-06 22:42:39 -0500

    reference Upstash Redis in code using environment variables

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -1,9 +1,9 @@
 import { Redis } from "@upstash/redis"
 
-const redisUrl = process.env.UPSTASH_REDIS_REST_URL || ""
-const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN || ""
+const url = process.env.UPSTASH_REDIS_REST_URL
+const token = process.env.UPSTASH_REDIS_REST_TOKEN
 
 export const redisClient = new Redis({
-  url: redisUrl,
-  token: redisToken
+  url,
+  token
 })
